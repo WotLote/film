3c364d498f22e45b35fee30ba369f1f4dd3d5312
WotLote <jerab36rum@gmail.com>
2019-06-21 20:31:44 +0400

    'firstupdate'

---
+++ src/Components/Film.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default () => {
+	return (
+		<div>
+			<div className="gg">
+				<iframe src="http://streamtojupiter.me/video/969a01d0c5e4460c/iframe?find_last_episode=1&amp;show_translations=1" width="610" height="370"></iframe>
+			</div>
+		</div>
+	)
+}
